be8f42a3fc6a208f6ed829c8efc89146ee05b76a
nicklas <nsenentz@hotmail.fr>
2020-12-18 10:14:58 +0100

    before form edit/add user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -46,11 +49,87 @@ class SecurityController extends AbstractController
          $this->denyAccessUnlessGranted('ROLE_ADMIN');
         // get all users
         $users =  $userRepository->findAll();
-   // display users
+        // display users
         return $this->render('security/allUsers.html.twig', [
             'users' => $users
         ]);
+    }
 
-     
+
+      /**
+     * @Route("/admin/user/add", name="ajoutUser")
+     */
+    public function addUser(EntityManagerInterface $em, Request $request)
+    {
+        // REstrictions admin
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+      
+        $user = new User;
+
+        $form = $this->createForm(ProductFormType::class, $user);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Le user a bien été créé ;)');
+
+            return $this->redirectToRoute('userList');
+        }
+
+        return $this->render('product/product-form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/edit/{id}", name="editUser")
+     */
+    // public function editUser(EntityManagerInterface $em, Request $request, $id)
+    // {
+
+    //      // REstrictions admin
+    //      $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+    //     //    Get user with id
+    //     $user = $em->getRepository(User::class)->find($id);
+
+    //     $form = $this->createForm(ProductFormType::class, $user);
+    //     $form->handleRequest($request);
+
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         // $product->setSlug($slugger->slug($product->getName()));
+
+    //         $em->persist($user);
+    //         $em->flush();
+    //         $this->addFlash('success', 'Modifications bien enregistrées ;)');
+    //         return $this->redirectToRoute('produits');
+    //     }
+
+    //     return $this->render('security/editUser.html.twig', [
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
+
+
+    /**
+     * @Route("/admin/user/delete/{id}", name="deleteUser")
+     */
+    public function deleteProduct(EntityManagerInterface $em,  $id)
+    {
+         // REstrictions admin
+         $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $user = $em->getRepository(User::class)->find($id);
+
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('success', 'Le user a bien été supprimé ;)');
+
+
+        return $this->redirectToRoute('userList');
     }
 }
