4e839ad9c1902858e13050857e581a43669b6c5e
nicklas <nsenentz@hotmail.fr>
2021-01-08 15:34:03 +0100

    CartController : working but not on empty cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,40 +3,57 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
     /**
      * @Route("/cart", name="cart")
      */
-    public function index(SessionInterface $session): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
-
+        // $session = new Session();
+        // $session->clear();
         $cart  = $session->get('cart', []);
+
+        $cart_view = array_count_values($cart);
       
+        foreach ($cart_view as $key => $value) {
+            $newCart[] = [
+                'product' => $productRepository->find($key),
+                'quantity'=> $value
+            ];
+        }
+
         return $this->render('cart/index.html.twig', [
-            'cart' => $cart,
+            'cart' => $newCart,
         ]);
     }
+    
 
-     /**
+    /**
      * @Route("/cart/add/{id}", name="addToCart")
      */
-    public function add(Request $request, SessionInterface $session, $id): Response
+    public function add(Request $request, SessionInterface $session, $id, ProductRepository $productRepository): Response
     {
 
         $cart  = $session->get('cart', []);
 
-        array_push($cart, $id);
-        $session->set('cart', $cart);
+        $arrayCart = (array) $cart;
+        array_push($arrayCart, $id);
+        $session->set('cart', $arrayCart);
 
         $this->addFlash('success', 'Produit ajouté au panier');
-
-        return $this->render('home.html.twig');
+        $products = $productRepository->findAll();
+        return $this->render('home.html.twig', [
+            'products' => $products
+        ]);
     }
 }
